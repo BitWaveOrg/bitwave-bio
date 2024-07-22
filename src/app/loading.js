202ecd0065c5bcbd44e1d53c28f6e734b7030b3f
v3sker <vespucci.samp@gmail.com>
2024-07-22 22:08:43 +0300

    chore: add shadow to profiles avatar

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
-export default function Loading() {
-    const styles = {
-        container: {
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh',
-            backgroundColor: '#000', // Темный фон
-        },
-    };
+export default function Loading()
+{const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    backgroundColor: '#000', // Темный фон
+  },
+};
 
-    return (
-        <div style={styles.container}>
-            <h1>Loading...</h1>
-        </div>
-    );
+  return (
+    <div style={styles.container}>
+      <h1>Loading...</h1>
+    </div>
+  );
 }
