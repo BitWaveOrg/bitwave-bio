1311af1477524d0733fdb0f0188a72ec24c3d217
v3sker <vespucci.samp@gmail.com>
2024-07-23 19:44:48 +0300

    chore: frontend internet rofls :sunglasses:

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { poppinsBold, jetbrainsMono } from "@/lib/fonts";
 
 export default function Home() {
     const [search, setSearch] = useState('');
@@ -16,18 +17,20 @@ export default function Home() {
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-50">
-            <h1 className="text-6xl font-bold mb-6 text-gray-900">Welcome to Our Project</h1>
-            <p className="text-xl text-gray-700 mb-12">
-                This is a brief description of the project. It aims to solve problems and provide unique features for our users.
+            <h1 className={`font-bold text-gray-900 ${poppinsBold.className}`}>Welcome to BitWave Bio! 🌊</h1>
+            <p className={`text-gray-700 mb-12 ${jetbrainsMono.className}`}>
+                Simple but powerful profile page-maker.
             </p>
             <form onSubmit={handleSearch} className="w-full max-w-md">
-                <div className="flex items-center border-b border-teal-500 py-2">
+                <div className="flex items-center border border-slate-300 rounded-md p-4  ">
                     <input
-                        className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                        className={`appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 
+                        leading-tight focus:outline-none ${jetbrainsMono.className}`}
                         type="text"
                         placeholder="Enter search term..."
                         value={search}
                         onChange={(e) => setSearch(e.target.value)}
+                        id={"main-search"}
                     />
                     <button
                         className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
