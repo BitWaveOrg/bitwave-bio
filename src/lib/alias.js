3430ec8014f1651381cc4d1e594549359d25ca3b
tuesday <andreysuew@gmail.com>
2024-07-23 21:32:21 +0200

    :sparkles: Add 'devops' alias to 'NellexBEST'.

--- src/lib/alias.js
+++ src/lib/alias.js
@@ -1,7 +1,8 @@
 const aliases = {
     'root': 'Rxflex',
     'dxrkyvo1d': 'dxrkyblood',
-    'nellex': 'NellexBEST'
+    'nellex': 'NellexBEST',
+    'devops': 'NellexBEST'
 }
 export function checkAlias (nickname) {
     return aliases[nickname] || nickname;
