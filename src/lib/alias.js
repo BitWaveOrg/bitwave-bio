d5d1d0743ca317d90d25f7a05c963e76d34b2198
tuesday <39628283+Rxflex@users.noreply.github.com>
2024-07-23 21:06:38 +0200

    Add new alias 'nellex' with value 'NellexBEST' to the aliases object.

--- src/lib/alias.js
+++ src/lib/alias.js
@@ -1,6 +1,7 @@
 const aliases = {
     'root': 'Rxflex',
     'dxrkyvo1d': 'dxrkyblood',
+    'nellex': 'NellexBEST'
 }
 export function checkAlias (nickname) {
     return aliases[nickname] || nickname;
